92bd6429cacf35e1c328ca916822f4920c669d7e
Navident <askdentalai@gmail.com>
2024-01-02 02:01:08 -0800

    fixed issue with proceduresCustomizer component rendering at same time as defaultproceduresmanagement

--- dentaltreatmentplanner.client/src/Components/DefaultProceduresManagement/DefaultProceduresManagement.jsx
+++ dentaltreatmentplanner.client/src/Components/DefaultProceduresManagement/DefaultProceduresManagement.jsx
@@ -5,6 +5,7 @@ import userIcon from '../../assets/user-icon.svg';
 import editIcon from '../../assets/pencil-edit-icon.svg';
 import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 import { useState, useEffect } from 'react';
@@ -65,43 +66,46 @@ const DefaultProcedures = () => {
     const handleEditClick = (subcategoryName) => {
         navigate(`/dashboard/defaultprocedures/procedurescustomizer/${subcategoryName}`);
     };
-
+    // Check if the current route is for editing a specific treatment plan
+    const isEditingTreatmentPlan = location.pathname.includes("/procedurescustomizer/");
 
     return (
-        <div className = "default-procedure-management-wrapper">
-            <Outlet /> 
-            <div className="large-text">Procedure Defaults</div>
-            
-            <div className="edit-procedures-container rounded-box">
-                
-                <div className="edit-procedures-inner">
-                    <div className="centered-title large-text">Procedure Categories</div>
-                    {categories.map(category => (
-                        <div className="table-container" key={category.procedureCategoryId}>
-                            <div className="edit-procedures-table-outer-header large-text">{category.name}</div>
-                            <table className="tx-table">
-                                <thead>
-                                    <tr className="table-inner-header">
-                                        <th>Sub-Categories</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {category.subCategories?.map(subCategory => (
-                                        <tr key={subCategory.procedureSubCategoryId}>
-                                            <td>
-                                                {subCategory.name}
-                                                <img src={editIcon} className="edit-button" alt="Edit" onClick={() => handleEditClick(subCategory.name)} />
-                                            </td>
+        isEditingTreatmentPlan ? (
+            <Outlet />
+        ) : (
+            <div className="default-procedure-management-wrapper">
+                <div className="large-text">Procedure Defaults</div>
+
+                <div className="edit-procedures-container rounded-box">
+                    <div className="edit-procedures-inner">
+                        <div className="centered-title large-text">Procedure Categories</div>
+                        {categories.map(category => (
+                            <div className="table-container" key={category.procedureCategoryId}>
+                                <div className="edit-procedures-table-outer-header large-text">{category.name}</div>
+                                <table className="tx-table">
+                                    <thead>
+                                        <tr className="table-inner-header">
+                                            <th>Sub-Categories</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    ))}
+                                    </thead>
+                                    <tbody>
+                                        {category.subCategories?.map(subCategory => (
+                                            <tr key={subCategory.procedureSubCategoryId}>
+                                                <td>
+                                                    {subCategory.name}
+                                                    <img src={editIcon} className="edit-button" alt="Edit" onClick={() => handleEditClick(subCategory.name)} />
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
-);
-};
+        )
+    );
 
+};
 export default DefaultProcedures;
