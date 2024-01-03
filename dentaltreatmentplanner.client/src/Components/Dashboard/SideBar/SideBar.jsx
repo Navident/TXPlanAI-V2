98c9db9318de9ba89e7820dd2ca5330ec8516885
Navident <askdentalai@gmail.com>
2024-01-03 14:58:42 -0800

    added logic for swapping to colored icon when active item

--- dentaltreatmentplanner.client/src/Components/Dashboard/SideBar/SideBar.jsx
+++ dentaltreatmentplanner.client/src/Components/Dashboard/SideBar/SideBar.jsx
@@ -4,11 +4,13 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 
 import homeIcon from '../../../assets/home-icon.svg';
+import homeIconActive from '../../../assets/home-icon-selected.svg';
 import newTxPlanIcon from '../../../assets/new-tx-plan-icon.svg';
 import savedTxPlansIcon from '../../../assets/saved-tx-plans-icon.svg';
 import providersIcon from '../../../assets/providers-icon.svg';
 import cdtCodesIcon from '../../../assets/cdt-codes-icon.svg';
 import settingsIcon from '../../../assets/tx-plan-settings-icon.svg';
+import settingsIconActive from '../../../assets/settings-icon-active.svg';
 import settingsIcon2 from '../../../assets/tx-plan-settings-icon.svg';
 
 const SideBar = () => {
@@ -17,13 +19,13 @@ const SideBar = () => {
     const [hoveredItem, setHoveredItem] = useState(null);
 
     const items = [
-        { imgSrc: homeIcon, text: "Home", path: "" },
-        { imgSrc: newTxPlanIcon, text: "New Tx Plan", path: "newTxPlan" },
-        { imgSrc: savedTxPlansIcon, text: "Saved Tx Plans", path: "savedTxPlans" },
-        { imgSrc: providersIcon, text: "Providers", path: "providers" },
-        { imgSrc: cdtCodesIcon, text: "Custom CDT Codes", path: "customizeCDTCodes" },
-        { imgSrc: settingsIcon, text: "TX Plan Settings", path: "defaultprocedures" },
-        { imgSrc: settingsIcon2, text: "Account Info", path: "accountInfo" }
+        { imgSrc: homeIcon, imgSrcSelected: homeIconActive, text: "Home", path: "" },
+        { imgSrc: newTxPlanIcon, imgSrcSelected: newTxPlanIcon, text: "New Tx Plan", path: "newTxPlan" },
+        { imgSrc: savedTxPlansIcon, imgSrcSelected: savedTxPlansIcon, text: "Saved Tx Plans", path: "savedTxPlans" },
+        { imgSrc: providersIcon, imgSrcSelected: providersIcon, text: "Providers", path: "providers" },
+        { imgSrc: cdtCodesIcon, imgSrcSelected: cdtCodesIcon, text: "Custom CDT Codes", path: "customizeCDTCodes" },
+        { imgSrc: settingsIcon, imgSrcSelected: settingsIconActive, text: "TX Plan Settings", path: "defaultprocedures" },
+        { imgSrc: settingsIcon2, imgSrcSelected: settingsIconActive, text: "Account Info", path: "accountInfo" }
 
     ];
 
@@ -64,7 +66,7 @@ const SideBar = () => {
                         onClick={() => handleItemClick(item.path)}
                         onMouseEnter={() => handleMouseEnter(index)}
                         onMouseLeave={handleMouseLeave}>
-                        <img src={item.imgSrc} alt={item.text} className="sidebar-icon" />
+                        <img src={isActive(item.path) ? item.imgSrcSelected : item.imgSrc} alt={item.text} className="sidebar-icon" />
                         <span className="sidebar-text">{item.text}</span>
                     </li>
                 ))}
