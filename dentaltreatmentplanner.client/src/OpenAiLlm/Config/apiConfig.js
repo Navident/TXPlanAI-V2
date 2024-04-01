50727c2831543c9f0655e277b0205bee4c3e2158
Navident <askdentalai@gmail.com>
2024-03-31 18:13:49 -0700

    updates ai model to gpt 4

--- dentaltreatmentplanner.client/src/OpenAiLlm/Config/apiConfig.js
+++ dentaltreatmentplanner.client/src/OpenAiLlm/Config/apiConfig.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     endpoint: "https://api.openai.com/v1/chat/completions",
-    model: "ft:gpt-3.5-turbo-0125:personal:playground-data:91MAKlHq",
+    model: "gpt-4-turbo-preview",
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
